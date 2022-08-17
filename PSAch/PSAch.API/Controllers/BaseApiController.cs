b0e298eef8f0b6232aaea78b33871ca631919a69
soullaft <soullaft@gmail.com>
2022-08-18 02:09:42 +0300

    added nlog

---
+++ PSAch/PSAch.API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PSAch.API.Controllers
+{
+    /// <summary>
+    /// Base API controller
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
--- PSAch/PSAch.API/Program.cs
+++ PSAch/PSAch.API/Program.cs
@@ -1,26 +1,40 @@
 using PSAch.API.Extensions.Services;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAplicationServices(builder.Configuration);
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddAplicationServices(builder.Configuration);
+
+    // NLog setup
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseCors(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(builder.Configuration.GetValue<string>("ClientUrl"));
+    });
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    await app.RunAsync();
+
 }
-
-app.UseCors(policy =>
-{
-    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(builder.Configuration.GetValue<string>("ClientUrl"));
-});
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-await app.RunAsync();
+catch(Exception ex) { logger.Error(ex); }
+finally { LogManager.Shutdown(); }
