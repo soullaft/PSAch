94a7825d4fdc860ec9242ffea8f7ad35dee41079
soullaft <soullaft@gmail.com>
2022-08-20 23:40:55 +0300

    added GamesRepository

---
+++ PSAch/PSAch.API/Data/GamesRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSAch.API.Models;
+
+namespace PSAch.API.Data
+{
+    public class GamesRepository : IBaseRepository<Game>
+    {
+        private readonly DataContext _context;
+
+        public GamesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Game>> GetAllAsync() => await _context.Games.ToListAsync();
+
+        public async Task<Game> GetByIdAsync(int id)
+        {
+            if(id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            return await _context.Games.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public Task<Game> UpdateAsync(Game entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PSAch/PSAch.API/Data/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace PSAch.API.Data
+{
+    public interface IBaseRepository <T> where T : class, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<int> SaveChangesAsync();
+    }
+}
