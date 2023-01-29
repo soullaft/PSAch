7eb99e7dc9b42ca3292018ee1b3a4eff3dbbc95b
soullaft <soullaft@gmail.com>
2023-01-29 22:40:32 +0300

    added GenerateHashSalt

--- PSAch/PSAch.API/Generator/Generator.cs
+++ PSAch/PSAch.API/Generator/Generator.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLog.Fluent;
+using PSAch.API.Data;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 
-namespace PSAch.API.Data.Generator
+namespace PSAch.API.Generator
 {
     public class Generator
     {
         private const string HARDCODED_PASSWORD = "1234";
 
-        public static async Task GenerateUsers(DataContext dataContext, IConfiguration configuration)
+        public static async Task GenerateUsersAsync(DataContext dataContext, IConfiguration configuration)
         {
             var generatedFilePath = configuration["GeneratedUserFile"];
             //if (await dataContext.Users.AnyAsync() && !File.Exists(generatedFilePath)) return;
@@ -17,6 +20,15 @@ namespace PSAch.API.Data.Generator
             //var users = JsonSerializer.Deserialize<ICollection<User>>(userdata);
 
             //foreach(var user in users)
+        }
+
+        public static (byte[], byte[]) GenerateHashSalt(string password)
+        {
+            using var hmac = new HMACSHA512();
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            var salt = hmac.Key;
+
+            return (hash, salt);
         }
     }
 }
