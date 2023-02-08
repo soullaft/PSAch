5e18d3e0cbbae0fe9b0b561779c7ea038997b004
soullaft <soullaft@gmail.com>
2023-02-08 03:02:04 +0300

    added models for mail

---
+++ PSAch/PSAch.API/Models/MailRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace PSAch.API.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; }
+        
+        public string Subject { get; set; }
+        
+        public string Body { get; set; }
+        
+        public IList<IFormFile> Attachments { get; set; }
+    }
+}
