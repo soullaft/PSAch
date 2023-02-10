52b1c274282525f4754a3d5835bccf6baef5aa90
soullaft <soullaft@gmail.com>
2023-02-11 02:56:53 +0300

    changed appuser props

--- PSAch/PSAch.Core/AppUser.cs
+++ PSAch/PSAch.Core/AppUser.cs
@@ -4,17 +4,17 @@ namespace PSAch.Core
 {
     public class AppUser : BaseModel
     {
-        public string? Login { get; set; }
+        public string Login { get; set; }
 
         public string? Nickname { get; set; }
 
-        public byte[]? PasswordHash { get; set; }
+        public byte[] PasswordHash { get; set; }
 
-        public byte[]? PasswordSalt { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
         public Gender Gender { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
         public string? About { get; set; }
 
@@ -24,7 +24,7 @@ namespace PSAch.Core
         [StringLength(30)]
         public string? Country { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public Photo? Photo { get; set; }
 
