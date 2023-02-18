7fd4ffc4a88e66772574ebbb47be9f1627ea0c55
soullaft <soullaft@gmail.com>
2023-02-18 04:00:14 +0300

    added config extension

---
+++ PSAch/PSAch.Telegram/Extensions/Services/AddConfigurationOptionsValuesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using PSAch.Telegram.Data;
+
+namespace PSAch.Telegram.Extensions.Services
+{
+    public static class AddConfigurationOptionsValuesExtensions
+    {
+        public static IServiceCollection AddConfigurationOptionsValues(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TelegramSettings>(configuration.GetSection("TelegramSettings"));
+
+            return services;
+        }
+    }
+}
