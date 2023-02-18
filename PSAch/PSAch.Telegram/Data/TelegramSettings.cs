4a344efb44e0f24ec8220e5aec56b3fd325ed856
soullaft <soullaft@gmail.com>
2023-02-18 03:56:56 +0300

    added settings class

---
+++ PSAch/PSAch.Telegram/Data/TelegramSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace PSAch.Telegram.Data
+{
+    public class TelegramSettings
+    {
+        public string Url { get; set; }
+
+        public string Name { get; set; }
+
+        public string Key { get; set; }
+    }
+}
